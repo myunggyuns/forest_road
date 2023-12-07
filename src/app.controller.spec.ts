13cb71e20e8a6b181a564bc7c4144b8e32b1ce3c
myunggyuns <sonmg90@gmail.com>
2023-12-07 22:13:05 +0900

    local test code 완료

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,23 +1,41 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn((key: string) => {
+              // this is being super extra, in the case that you need multiple keys with the `get` method
+              if (key === 'PORT') {
+                return 3000;
+              }
+              return null;
+            }),
+          },
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get(AppService);
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Heart Beat!"', () => {
+      appService.heartBeat;
       expect(appController.heartBeat()).toBe(
-        `Listen to my Heart Beat! PORT: ${8080}`,
+        `Listen to my Heart Beat! PORT: ${3000}`,
       );
     });
   });
